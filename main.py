a6ae24091168c14610067ef67f98d924fb6a3ce5
Tavis <72011296+TGSHOREQQ@users.noreply.github.com>
2020-09-30 18:11:20 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ from sklearn.utils import resample
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, plot_confusion_matrix, precision_score, recall_score, f1_score, \
-    roc_auc_score, roc_curve
+from sklearn.metrics import accuracy_score, plot_confusion_matrix, precision_score, recall_score, roc_auc_score, \
+    roc_curve
 import time
+
 # set pycharm terminal wider than default
 pd.set_option('display.width', 400)
 pd.set_option('display.max_columns', 14)
@@ -18,22 +19,51 @@ PATH = '/home/tav/Desktop/Untitled Folder/data/'
 dataset = pd.read_csv(f'{PATH}train.csv')
 test_data = pd.read_csv(f'{PATH}test.csv')
 
-print(train_data.info())
-print(train_data.describe())
-remove_cols = ['Ticket', 'Cabin', 'Cabin']
 
+# Function for fitting model, calculating metrics and adding to ROC plot
+# global variables used as parameters. Improvement?
+def create_model(model_type, X_train, y_train, X_test, y_test, roc_plot):
+    model_name = type(model_type).__name__
+    time_start = time.perf_counter()
+    model = model_type.fit(X_train, y_train)
+    time_elapsed = (time.perf_counter() - time_start)
+
+    prob = model_type.predict_proba(X_test)[:, 1]
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred) * 100
+    precision = precision_score(y_test, y_pred) * 100
+    recall = recall_score(y_test, y_pred) * 100
+    auc = roc_auc_score(y_test, prob)
+    fpr, tpr, _ = roc_curve(y_test, prob)
+
+    print(f"{model_name} Metrics")
+    print("Computation Time:%5.4f seconds" % time_elapsed)
+    print("Accuracy: %.2f" % accuracy)
+    print("Precision: %.2f" % precision)
+    print("Recall: %.2f" % recall)
+    print('AUC=%.3f' % auc, "\n")
+    confusion_matrix(model, model_name)
+    roc_plot.plot(fpr, tpr, marker='.', label=f'{model_name}')
+
+
+# Plots confusion matrix for ML model
+def confusion_matrix(model, model_name):
+    plot_confusion_matrix(model, X_test, y_test, normalize='true')
+    plt.title(f'{model_name} Confusion Matrix')
+
+# No stand out data to extract from Ticket or Cabin that won't just correlate to Class/Fare
+remove_cols = ['Ticket', 'Cabin']
 numeric = ['Age', 'SibSp', 'Parch', 'Fare']  # histograms
 categorical = ['Pclass', 'Sex', 'Embarked']  # valuecount()
 
 for i in numeric:
-     plt.hist(dataset[i])
-     plt.title(i)
-     plt.show()
-
+    plt.hist(dataset[i])
+    plt.title(i)
+    plt.show()
 for i in categorical:
-     plt.bar(dataset[i].value_counts().index, dataset[i].value_counts())
-     plt.title(i)
-     plt.show()
+    plt.bar(dataset[i].value_counts().index, dataset[i].value_counts())
+    plt.title(i)
+    plt.show()
 
 titles = []
 for person in dataset['Name']:
@@ -81,112 +111,29 @@ dataset[['Age']] = age_normalised
 df_majority = dataset[dataset['Survived'] == 0]
 df_minority = dataset[dataset['Survived'] == 1]
 
-# Downsample majority class
-df_majority_downsampled = resample(df_majority, replace=False, n_samples=len(dataset[dataset['Survived'] == 1]),
-                                   random_state=123)
-df_downsampled = pd.concat([df_majority_downsampled, df_minority])
-df_downsampled = df_downsampled.sample(frac=1, random_state=30).reset_index().drop(columns=['index'])
-
 # Upsample minority class
 df_minority_upsampled = resample(df_minority, replace=True, n_samples=len(dataset[dataset['Survived'] == 0]),
                                  random_state=123)
 df_upsampled = pd.concat([df_majority, df_minority_upsampled])
 df_upsampled = df_upsampled.sample(frac=1, random_state=30).reset_index().drop(columns=['index'])
 
-# no na values now
-# Subset data into train and test
-# X_train, X_test, y_train, y_test = train_test_split(df_downsampled.loc[:, df_downsampled.columns != 'Survived'],
-#                                                     df_downsampled['Survived'], test_size=0.3, random_state=2)
 X_train, X_test, y_train, y_test = train_test_split(df_upsampled.loc[:, df_upsampled.columns != 'Survived'],
                                                     df_upsampled['Survived'], test_size=0.3, random_state=2)
 
+f1 = plt.figure()
+ax1 = f1.add_subplot(111)
 
-# Plot Results
-fig = plt.figure(figsize=(14, 4))
-(ax1, ax2, ax3) = fig.subplots(1, 3)
-fig.suptitle('ML Model Confusion Matrices', size=16)
+models = [LogisticRegression(max_iter=1000), DecisionTreeClassifier(max_depth=10),
+          RandomForestClassifier(max_depth=12, random_state=10)]
+for model in models:
+    create_model(model, X_train, y_train, X_test, y_test, ax1)
 
-# Logistic Regression
-lr = LogisticRegression(n_jobs=-1, max_iter=1000)
-time_start_lr = time.perf_counter()
-lr_model = lr.fit(X_train, y_train)
-time_elapsed_lr = (time.perf_counter() - time_start_lr)
-# Model Values
-y_pred_lr = lr_model.predict(X_test)
-prob_lr = lr.predict_proba(X_test)[:, 1]
-lr_accuracy = accuracy_score(y_test, y_pred_lr) * 100
-lr_precision = precision_score(y_test, y_pred_lr) * 100
-lr_recall = recall_score(y_test, y_pred_lr) * 100
-plot_confusion_matrix(lr_model, X_test, y_test, ax=ax1, normalize='pred')
-ax1.title.set_text('Logistic Regression Confusion Matrix')
+ax1.plot([0, 1], [0, 1], 'r--')
 
-# Decision Tree
-dt = DecisionTreeClassifier(max_depth=10, random_state=1)
-time_start_dt = time.perf_counter()
-dt_model = dt.fit(X_train, y_train)
-time_elapsed_dt = (time.perf_counter() - time_start_dt)
-# Model Values
-y_pred_dt = dt_model.predict(X_test)
-prob_dt = dt.predict_proba(X_test)[:, 1]
-dt_accuracy = accuracy_score(y_test, y_pred_dt) * 100
-dt_precision = precision_score(y_test, y_pred_dt) * 100
-dt_recall = recall_score(y_test, y_pred_dt) * 100
-plot_confusion_matrix(dt_model, X_test, y_test, ax=ax2, normalize='pred')
-ax2.title.set_text('Decision Tree Confusion Matrix')
-
-# Random Forest
-rf = RandomForestClassifier(max_depth=12, random_state=10)
-time_start_rf = time.perf_counter()
-rf_model = rf.fit(X_train, y_train)
-time_elapsed_rf = (time.perf_counter() - time_start_rf)
-# Model Values
-y_pred_rf = rf_model.predict(X_test)
-prob_rf = rf.predict_proba(X_test)[:, 1]
-rf_accuracy = accuracy_score(y_test, y_pred_rf) * 100
-rf_precision = precision_score(y_test, y_pred_rf) * 100
-rf_recall = recall_score(y_test, y_pred_rf) * 100
-plot_confusion_matrix(rf_model, X_test, y_test, ax=ax3, normalize='pred')
-ax3.title.set_text('Random Forest Confusion Matrix')
-plt.show()
-
-# ROC Curves
-auc_lr = roc_auc_score(y_test, prob_lr)
-auc_dt = roc_auc_score(y_test, prob_dt)
-auc_rf = roc_auc_score(y_test, prob_rf)
-
-lr_fpr, lr_tpr, _ = roc_curve(y_test, prob_lr)
-dt_fpr, dt_tpr, _ = roc_curve(y_test, prob_dt)
-rf_fpr, rf_tpr, _ = roc_curve(y_test, prob_rf)
-
-plt.plot([0, 1], [0, 1], 'r--')
-plt.plot(lr_fpr, lr_tpr, marker='.', label='Logistic Regression')
-plt.plot(dt_fpr, dt_tpr, marker='.', label='Decision Tree')
-plt.plot(rf_fpr, rf_tpr, marker='.', label='Random Forest')
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
-plt.legend()
+ax1.legend()
 plt.show()
 
-# Logistic Regression Metrics
-print("Logistic Regression Metrics")
-print("LR Computation Time:%5.4f seconds" % time_elapsed_lr)
-print("LR Accuracy: %.4f" % lr_accuracy)
-print("LR Precision: %.4f" % lr_precision)
-print("LR Recall: %.4f" % lr_recall)
-print("LR AUC: %.4f" % auc_lr, "\n")
 
-# Decision Tree Metrics
-print("Decision Tree Metrics")
-print("DT Computation Time:%5.4f seconds" % time_elapsed_dt)
-print("DT Accuracy: %.4f" % dt_accuracy)
-print("DT Precision: %.4f" % dt_precision)
-print("DT Recall: %.4f" % dt_recall)
-print("DT AUC: %.4f" % auc_dt, "\n")
-
-# Random Forest Metrics
-print("Decision Tree Random Forest Metrics")
-print("DTRF Computation Time:%5.4f seconds" % time_elapsed_rf)
-print("DTRF Accuracy: %.4f" % rf_accuracy)
-print("DTRF Precision: %.4f" % rf_precision)
-print("DTRF Recall: %.4f" % rf_recall)
-print("DTRF AUC: %.4f" % auc_rf)
+plt.show()
